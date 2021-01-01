515ef66e36475894d54218a3785b95d3a55d6c97
Darshsys <70889528+Darshsys@users.noreply.github.com>
2021-01-01 18:27:39 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,8 +8,8 @@ function preload(){
 
 function setup(){
 	createCanvas(500,500);
-  //foodStock=database.ref('Food');
-  //foodStock.on("value",readStock)
+  foodStock=database.ref('Food');
+  foodStock.on("value",readStock)
 }
 
 function draw(){  
@@ -19,8 +19,27 @@ function draw(){
     dog.addImage(dogHappy);
   }
 
+  readStock();
+  writeStock();
+
   drawSprites();
   text("Food Remaning : 20",200,210);
   textSize(50);
   fill(255,255,255);
+}
+
+function readStock(data){
+  foodS=data.val();
+}
+
+function writeStock(x){
+  if(x<=0){
+    x=0;
+  }else{
+    x=x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
 }
