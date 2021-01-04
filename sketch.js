34c759016e99a63e0b2fa6a18e08e91fb5c60bed
Darshsys <70889528+Darshsys@users.noreply.github.com>
2021-01-04 15:02:22 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -8,24 +8,31 @@ function preload(){
 
 function setup(){
 	createCanvas(500,500);
+  database=firebase.database();
   foodStock=database.ref('Food');
   foodStock.on("value",readStock)
+
+  dog=createSprite(250,300,150,150);
+  dog.addImage(dogSprite);
+  dog.scale=0.15;
+  //happydog=createSprite(250,300,150,150);
+
 }
 
 function draw(){  
   background(46,139,87);
   if (keyWentDown(UP_ARROW)){
     writeStock(foodS);
-    dog.addImage(dogHappy);
+    dog.addImage(happyDogSprite);
   }
 
-  readStock();
-  writeStock();
+  //readStock();
+  //writeStock();
 
   drawSprites();
-  text("Food Remaning : 20",200,210);
-  textSize(50);
+  textSize(20);
   fill(255,255,255);
+  text("Food Remaning : "+foodS,170,210);
 }
 
 function readStock(data){
